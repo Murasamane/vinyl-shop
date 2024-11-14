8d24054648cda3490adcadedba7bbfec5647d788
Giorgi Gurgenidze <77699030+Murasamane@users.noreply.github.com>
2024-11-15 00:51:43 +0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -55,6 +55,6 @@ async function bootstrap() {
 
   app.use(passport.initialize());
   app.use(passport.session());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, hostname:'0.0.0.0');
 }
 bootstrap();
